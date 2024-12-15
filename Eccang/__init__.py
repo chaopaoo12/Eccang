b7b073fc38fb86553ddbc3f27e456c7d71198b2b
chaopaoo12 <chaopaoo12@hotmail.com>
2024-12-15 23:15:08 +0800

    modified:   Eccang/__init__.py

---
+++ Eccang/__init__.py
@@ -0,0 +1,13 @@
+# -*- encoding: utf-8 -*-
+'''
+@File    :   __init__.py
+@Time    :   2024/12/14 14:13:16
+@Author  :   chaopaoo12 
+@Version :   1.0
+@Contact :   chaopaoo12@hotmail.com
+'''
+
+# here put the import lib
+
+from base_fetch import *
+from fetch_record import *
