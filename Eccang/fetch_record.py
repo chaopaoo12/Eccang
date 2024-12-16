ba055af21e167b229a428916e8a9d5150e546319
chaopaoo12 <chaopaoo12@hotmail.com>
2024-12-16 10:34:19 +0800

    modified:   Eccang/fetch_record.py

--- Eccang/fetch_record.py
+++ Eccang/fetch_record.py
@@ -11,6 +11,17 @@
 from Eccang.eccang_base import eccang
 
 
+def getProductBarcodeMapList(path='setting.json', warehouse_code=None):
+    # listing表现-日维度接口
+    EC = eccang(path)
+    ListingSummaryOriginal = EC.get_data(interface_name='getProductBarcodeMapList',
+                                         biz_content={'page':1,'page_size':1000,
+                                                      'warehouse_code':warehouse_code
+                                                      },
+                                         data_format='dataframe')
+    return (ListingSummaryOriginal)
+
+
 def getListingSummaryOriginal(path='setting.json', end_date=None, create_date=None):
     # listing表现-日维度接口
     EC = eccang(path)
